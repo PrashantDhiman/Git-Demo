bef8710c4f2098514eb86590eb18099b777e7842
Prashant Dhiman <prashantdhiman234@gmail.com>
2020-02-21 12:57:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+MaxHealth Web App
+
+Created by: Prashant Dhiman(41),
+            Prateek Gupta(42),
+            Rasesh Koul(43),
+            Sahil(50)
---
+++ app.js
@@ -0,0 +1,35 @@
+var express=require("express");
+var app=express();
+var port=3000;
+
+// Landing page route
+app.get("/", function(req, res){
+    res.render("landing.ejs");
+});
+
+// Index page route
+// OTC -> Over The Counter(Non-Prescription drugs)
+app.get("/otc", function(req, res){
+    res.render("otc.ejs");
+});
+
+// Show OTC product route
+app.get("/otc/:id", function(req, res){
+    res.render("show.ejs");
+});
+
+// Login page route
+app.get("/login", function(req, res){
+    res.render("login.ejs");
+});
+
+// Sign Up page route
+app.get("/signup", function(req, res){
+    res.render("signup.ejs");
+});
+
+
+
+app.listen(port, function(){
+    console.log("server started on port " + port);
+});
